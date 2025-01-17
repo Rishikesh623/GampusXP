483af69152d4cab3a065b3a410e0aff997fad480
ELBERLIN <banditrohit05@gmail.com>
2025-01-17 18:38:31 +0530

    Added aura points in main

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -6,6 +6,7 @@ const userSlice = createSlice({
         name: '',
         reg_no: '',
         email: '',
+        aura_points: 0
     },
     reducers: {
         setUserProfile: (state, action) => {
@@ -13,6 +14,7 @@ const userSlice = createSlice({
             state.reg_no = action.payload.reg_no;
             state.email = action.payload.email;
             state.password = action.payload.password;
+            state.aura_points = action.payload.aura_points;
         },
         updateRegNo: (state, action) => {
             state.reg_no = action.payload;
@@ -20,11 +22,14 @@ const userSlice = createSlice({
         updateEmail: (state, action) => {
             state.email = action.payload;
         },
+        updateAuraPoints: (state, action) => {
+            state.aura_points = action.payload;
+        },
         logout: (state) => {
-            return { name: null, email: null, reg_no: null };
+            return { name: null, email: null, reg_no: null, aura_points: 0 };
         },
     }
 })
 
-export const { setUserProfile, updateEmail, updateRegNo, logout } = userSlice.actions;
+export const { setUserProfile, updateEmail, updateRegNo, updateAuraPoints, logout } = userSlice.actions;
 export default userSlice.reducer;
